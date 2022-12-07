0faae82339b4916b3ebe92ad414a1f5ea51c02c2
Documenter.jl <documenter@juliadocs.github.io>
2022-12-07 05:32:51 +0000

    build based on 2e64ed5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = WASMTools","category":"page"},{"location":"#WASMTools","page":"Home","title":"WASMTools","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for WASMTools.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [WASMTools]","category":"page"}]
+}
