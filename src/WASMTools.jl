2de10fa80eb4c69cd0850867b0fdb3cc3c7018d6
arhik <arhik23@gmail.com>
2022-12-07 10:23:34 +0530

    Reinit commit

--- src/WASMTools.jl
+++ src/WASMTools.jl
@@ -1,5 +1,16 @@
 module WASMTools
 
 # Write your package code here.
+using LLVM
+using LLVM.Interop
+
+using ExprTools: splitdef, combinedef
+using TimerOutputs
+using Logging
+using UUIDs
+using Libdl
+
+include("runtime.jl")
+include("target.jl")
 
 end
